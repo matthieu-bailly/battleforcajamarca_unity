2b78716e4827bc0411ac198b75120ac1b0a91ee7
matthieu-bailly <baillymatthieu78@yahoo.fr>
2018-02-28 23:55:41 +0100

    Premiers test Unity
    Grid complete

---
+++ battle_for_cajamarca/Assets/scripts/CameraTactique.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTactique : MonoBehaviour {
+
+	public void RotationGauche()
+	{
+		transform.Rotate (Vector3.up, 90, Space.Self);
+	}
+
+	public void RotationDroite()
+	{
+		transform.Rotate (Vector3.up, -90, Space.Self);
+	}
+
+}
---
+++ battle_for_cajamarca/Assets/scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuScript {
+
+	[MenuItem("Tools/Assign Tile Script")]
+	public static void AssignTileScript()
+	{
+		GameObject[] tiles = GameObject.FindGameObjectsWithTag ("Tile");
+
+		foreach (GameObject t in tiles) {
+			t.AddComponent<Tile> ();
+		}
+	}
+}
